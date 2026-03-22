2a113afc58c7ad606024adbc01f77e66d5d52bb3
Zackary Frazier <frazier.z@northeastern.edu>
2026-03-21 19:46:47 -0700

    updated record.sh to resume

--- scripts/ur12e/record.sh
+++ scripts/ur12e/record.sh
@@ -1,4 +1,4 @@
-rm -rf /home/zackary-frazier/GitHub/EECE5552_Course_Project/datasets/ur12e_sim_dataset
+# rm -rf /home/zackary-frazier/GitHub/EECE5552_Course_Project/datasets/ur12e_sim_dataset
 lerobot-record \
     --robot.type=ur12e_ros \
     --teleop.type=ros_twist \
@@ -10,4 +10,4 @@ lerobot-record \
     --dataset.reset_time_s=20 \
     --dataset.single_task="pick up imaginary block" \
     --robot.cameras="{ top: {type: opencv, index_or_path: 'ros:///cameras/top/image_raw', width: 640, height: 480, fps: 30 }, side: {type: opencv, index_or_path: 'ros:///cameras/side/image_raw', width: 640, height: 480, fps: 30 }, front: {type: opencv, index_or_path: 'ros:///cameras/front/image_raw', width: 640, height: 480, fps: 30 } }" \
-    --resume false
+    --resume true
